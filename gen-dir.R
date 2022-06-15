66f3597cd9f704a510fe80b9fb2f316316b47899
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2022-06-15 22:16:35 +0200

    initial repo setup
    initial folder structure set up

---
+++ gen-dir.R
@@ -0,0 +1,11 @@
+#create directories
+dir.create("./R")
+dir.create("./R/subscripts")
+dir.create("./R/figures")
+dir.create("./R/functions")
+
+dir.create("./data")
+dir.create("./data/raw")
+dir.create("./data/processed")
+
+dir.create("./manuscript")
