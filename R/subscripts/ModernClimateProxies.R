9fa82baabd9fb4d2ea794ff45e46ec1c8c2d1708
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2022-07-12 14:11:27 +0200

    update to ModernClimateProxies

--- R/subscripts/ModernClimateProxies.R
+++ R/subscripts/ModernClimateProxies.R
@@ -1,12 +1,25 @@
+# Load libraries ----------------------------------------------------------
 library(raster)
 library(sf)
-
+# Coral Reefs -------------------------------------------------------------
 # Mean Annual Sea Surface Temperature
-# Source: https://www.bio-oracle.org/index.php
+# Source:
+# https://www.bio-oracle.org/index.php
 sst <- raster("./data/raw/climate/BioOracle_20220711/Present.Surface.Temperature.Mean.asc")
-vals <- getValues(sst)
-hist(vals, main = "Available sea surface temperature",
-     xlab = "Mean sea surface temperature (\u00b0C)")
+# Generate shallow marine mask for coral reefs
+# ETOPO5 world digital elevation model
+# Source:
+# https://www.eea.europa.eu/data-and-maps/data/world-digital-elevation-model-etopo5
+dem <- raster("./data/raw/DEM/alwdgg.tif")
+dem[dem > 0] <- NA
+dem <- abs(dem)
+dem[dem > 200] <- NA
+#mask sst layer by available shallow marine habitat
+sst <- mask(x = sst, mask = dem)
+vals_sst <- getValues(sst)
+hist(vals_sst, main = "Available sea surface temperature (continental shelf)",
+     xlab = "Mean sea surface temperature (\u00b0C)",
+     col=rgb(1,0,0,0.5))
 
 # Load coral reef data 
 # Source: https://data.unep-wcmc.org/datasets/1
@@ -16,13 +29,34 @@ reef <- sf::read_sf(
 reef <- sf::st_coordinates(reef)
 cells <- unique(raster::cellFromXY(object = sst, xy = reef))
 reef <- xyFromCell(object = sst, cell = cells)
-vals <- sst[cells]
-hist(vals, main = "Warm-water coral reefs",
-     xlab = "Mean sea surface temperature (\u00b0C)")
-reef <- cbind(reef, vals)
+vals_reef <- sst[cells]
+hist(vals_reef, main = "Warm-water coral reefs",
+     xlab = "Mean sea surface temperature (\u00b0C)",
+     col=rgb(0,0,1,0.5))
+reef <- cbind(reef, vals_reef)
 colnames(reef) <- c("lng", "lat", "sst")
 reef <- na.omit(reef)
 saveRDS(reef, "./data/processed/modern_reef_temp.RDS")
+
+# Mangroves ---------------------------------------------------------------
+# Mean Annual Sea Surface Temperature
+# Source:
+# https://www.bio-oracle.org/index.php
+sst <- raster("./data/raw/climate/BioOracle_20220711/Present.Surface.Temperature.Mean.asc")
+# Generate shallow marine mask for coral reefs
+# ETOPO5 world digital elevation model
+# Source:
+# https://www.eea.europa.eu/data-and-maps/data/world-digital-elevation-model-etopo5
+dem <- raster("./data/raw/DEM/alwdgg.tif")
+dem[dem > 10] <- NA
+dem <- abs(dem)
+dem[dem > 10] <- NA
+#mask sst layer by available shallow marine habitat
+sst <- mask(x = sst, mask = dem)
+vals_sst <- getValues(sst)
+hist(vals_sst, main = "Available sea surface temperature (coastlines)",
+     xlab = "Mean sea surface temperature (\u00b0C)",
+     col=rgb(1,0,0,0.5))
 
 # Load mangrove data 
 # Source: https://data.unep-wcmc.org/datasets/5
@@ -32,10 +66,11 @@ mangrove <- sf::read_sf(
 mangrove <- sf::st_coordinates(mangrove)
 cells <- unique(raster::cellFromXY(object = sst, xy = mangrove))
 mangrove <- xyFromCell(object = sst, cell = cells)
-vals <- sst[cells]
-hist(vals, main = "Mangroves",
-     xlab = "Mean sea surface temperature (\u00b0C)")
-mangrove <- cbind(mangrove, vals)
+vals_mangrove <- sst[cells]
+hist(vals_mangrove, main = "Mangroves",
+     xlab = "Mean sea surface temperature (\u00b0C)",
+     col=rgb(0,1,0,0.5))
+mangrove <- cbind(mangrove, vals_mangrove)
 colnames(mangrove) <- c("lng", "lat", "sst")
 mangrove <- na.omit(mangrove)
 saveRDS(mangrove, "./data/processed/modern_mangrove_temp.RDS")
