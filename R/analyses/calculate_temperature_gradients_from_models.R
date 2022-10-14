4f423db92b5e1f3d27c9981e39241c1b105ce1e1
Kilian Eichenseer <kilian.eichenseer@gmail.com>
2022-10-14 12:41:32 +0100

    gradient analysis for results

--- R/analyses/calculate_temperature_gradients_from_models.R
+++ R/analyses/calculate_temperature_gradients_from_models.R
@@ -6,8 +6,7 @@ modm <- readRDS("./results/modern/modern_climate_model_output.rds")
 
 # eocene
 mode <- readRDS("./results/eeco/eeco_climate_model_output_combined.rds")
-# combine chains, discard burnin
-mode <- combine_posterior(mode,100000)
+
 
 # calculate temperatures at lat 0 and lat 90 for every iteration
 modern_t0 <- gradient(0,modm[,1:4])
@@ -20,13 +19,11 @@ n <- length(modern_t0)
 
 # randomly recombine iterations of modern and eocene gradient and take difference
 set.seed(1)
-quantile(eocene_t0[sample(1:n,n)] - modern_t0[sample(1:n,n)], probs = c(0.025,0.5,0.975))
-quantile(eocene_t90[sample(1:n,n)] - modern_t90[sample(1:n,n)], probs = c(0.025,0.5,0.975))
+tdiff_lat0 <- as.list(quantile(eocene_t0[sample(1:n,n)] - modern_t0[sample(1:n,n)], probs = c(0.025,0.5,0.975)))
+tdiff_lat90 <- as.list(quantile(eocene_t90[sample(1:n,n)] - modern_t90[sample(1:n,n)], probs = c(0.025,0.5,0.975)))
 
 # calculate overall gradient for modern and eocene (polar - equatorial difference)
-quantile(eocene_t0 - eocene_t90, probs = c(0.025,0.5,0.975))
-quantile(modern_t0 - modern_t90, probs = c(0.025,0.5,0.975))
+eocene_gradient <- as.list(quantile(eocene_t0 - eocene_t90, probs = c(0.025,0.5,0.975)))
+modern_gradient <- as.list(quantile(modern_t0 - modern_t90, probs = c(0.025,0.5,0.975)))
 
-# calculate global average temperatures
-
-(2*pi*1^2)*(cos(90-40)-cos(90-0))
+# save results
