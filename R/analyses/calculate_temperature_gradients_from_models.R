de1d31b1fb994c920000aebf30165ab513212ce4
Kilian Eichenseer <kilian.eichenseer@gmail.com>
2023-01-24 21:29:47 +0000

    results updates

--- R/analyses/calculate_temperature_gradients_from_models.R
+++ R/analyses/calculate_temperature_gradients_from_models.R
@@ -11,15 +11,22 @@ mode <- readRDS("./results/eeco/eeco_climate_model_output_combined.rds")
 # modern with eocene sampling
 modem_list <- readRDS("./results/modern/modern_sample_eocene_p_lat_gradient.rds")
 modem <- combine_posterior(modem_list,5000)
-  
+
+# just proxy
+eeco_prox <- readRDS("results/eeco/eeco_climate_model_output_just_proxy.rds")
+eeco_prox <- combine_posterior(eeco_prox, burnin = 100000)
+
+
 # calculate temperatures at lat 0 and lat 90 for every iteration
 modern_t0 <- gradient(0,modm[,1:4])
 modern_eocene_t0 <- gradient(0,modem[,1:4])
 eocene_t0 <- gradient(0,mode[,1:4])
+eeco_prox_t0 <- gradient(0,eeco_prox[,1:4])
 
 modern_t90 <- gradient(90,modm[,1:4])
 modern_eocene_t90 <- gradient(90,modm[,1:4])
 eocene_t90 <- gradient(90,mode[,1:4])
+eeco_prox_t90 <- gradient(90,eeco_prox[,1:4])
 
 n <- length(modern_t0)
 
@@ -32,6 +39,12 @@ tdiff_lat90 <- as.list(format(round(quantile(eocene_t90[sample(1:n,n)] - modern_
 eocene_gradient <- as.list(format(round(quantile(eocene_t0 - eocene_t90, probs = c(0.025,0.5,0.975)),1),nsmall = 1))
 modern_gradient <- as.list(format(round(quantile(modern_t0 - modern_t90, probs = c(0.025,0.5,0.975)),1),nsmall = 1))
 modern_eocene_gradient <- as.list(format(round(quantile(modern_eocene_t0 - modern_eocene_t90, probs = c(0.025,0.5,0.975)),1),nsmall = 1))
+eocene_proxy_gradient <- as.list(format(round(quantile(eeco_prox_t0 - eeco_prox_t90, probs = c(0.025,0.5,0.975)),1),nsmall = 1))
+
+eocene_gradient_uncertainty <- quantile(eocene_t0 - eocene_t90, probs = c(0.975))-quantile(eocene_t0 - eocene_t90, probs = c(0.025))
+eocene_proxy_gradient_uncertainty <- quantile(eeco_prox_t0 - eeco_prox_t90, probs = c(0.975))-quantile(eeco_prox_t0 - eeco_prox_t90, probs = c(0.025))
+
+proxy_uncertain_diff_percent <- as.list(format((round(eocene_proxy_gradient_uncertainty/eocene_gradient_uncertainty,2)-1)*100,nsmall = 0))
 
 # eocene sample deviation from modern gradient
 modern_eocene_gradient_diff <- as.list(format(round(quantile((modern_eocene_t0 - modern_eocene_t90)-(modern_t0 - modern_t90), probs = c(0.025,0.5,0.975)),1),nsmall = 1))
@@ -40,6 +53,9 @@ modern_eocene_gradient_diff <- as.list(format(round(quantile((modern_eocene_t0 -
 saveRDS(tdiff_lat0, "./results/eeco/eocene_modern_difference_equator.rds")
 saveRDS(tdiff_lat90, "./results/eeco/eocene_modern_difference_poles.rds")
 saveRDS(eocene_gradient, "./results/eeco/eocene_overall_gradient.rds")
+saveRDS(eocene_proxy_gradient, "./results/eeco/eocene_proxy_gradient.rds")
+
 saveRDS(modern_gradient, "./results/modern/modern_overall_gradient.rds")
 saveRDS(modern_eocene_gradient, "./results/modern/modern_eocene_sampling_overall_gradient.rds")
 saveRDS(modern_eocene_gradient_diff, "./results/modern/modern_eocene_sampling_overall_gradient_diff.rds")
+saveRDS(proxy_uncertain_diff_percent, "./results/eeco/proxy_vs_full_uncertain_diff_percent.rds")
--- R/analyses/global_mean_temperatures.R
+++ R/analyses/global_mean_temperatures.R
@@ -1,11 +1,16 @@
 # global mean temperatures for the modern and the Eocene
 
 source("R/functions/model_components/gradient.R")
+source("R/functions/auxiliary_functions.R")
+
 mode_all <- readRDS("results/eeco/eeco_climate_model_output_combined.rds")
 modm_all <- readRDS("results/modern/modern_climate_model_output.rds")
 # modern with eocene sampling
 modem_list <- readRDS("./results/modern/modern_sample_eocene_p_lat_gradient.rds")
 modem_all <- combine_posterior(modem_list,5000)
+# just proxy
+eeco_prox <- readRDS("results/eeco/eeco_climate_model_output_just_proxy.rds")
+eeco_prox <- combine_posterior(eeco_prox, burnin = 100000)
 
 # latitudinal weights in 1 deg lat bands
 alpha1 <- seq(1,90,1)
@@ -30,15 +35,23 @@ moeotempq <- apply(moeotemp,2,function(x) quantile(x, probs = c(0.025,0.5,0.975)
 # modern eocene sampled global average temperature with 95% CI
 moeotemp_global <- apply(moeotempq,1, function(x) round(sum(x*latweight),1), simplify = FALSE)
 #
+# eocene proxy only temperature gradient
+eotempprox <- gradient(seq(0.5,89.5,1), eeco_prox[,1:4],0)
+
 
 moeotemp_global_all <- apply(moeotemp,1, function(x) (sum(x*latweight)), simplify = T)
 motemp_global_all <- apply(motemp,1, function(x) (sum(x*latweight)), simplify = T)
-
+eotemp_global_all <- apply(eotemp,1, function(x) (sum(x*latweight)), simplify = T)
+eotempprox_global_all <- apply(eotempprox,1, function(x) (sum(x*latweight)), simplify = T)
 # modern eocene sample deviation of global average
 modern_eocene_gradient_deviation <- as.list(format(round(quantile(moeotemp_global_all-motemp_global_all, probs = c(0.025,0.5,0.975)),1),nsmall = 1))
+
+# eocene proxy deviation of all eocene average
+eocene_eoceneproxy_gradient_deviation <- as.list(format(round(quantile(eotempprox_global_all-eotemp_global_all, probs = c(0.025,0.5,0.975)),1),nsmall = 1))
 
 # save results
 saveRDS(motemp_global,"results/modern/global_mean_modern.rds")
 saveRDS(eotemp_global,"results/eeco/global_mean_eeco.rds")
 saveRDS(moeotemp_global,"results/eeco/global_mean_modern_eocene_sampling_eeco.rds")
 saveRDS(modern_eocene_gradient_deviation,"results/eeco/global_mean_modern_eocene_sampling_difference.rds")
+saveRDS(eocene_eoceneproxy_gradient_deviation,"results/eeco/global_mean_eocene_eoceneproxy_difference.rds")
