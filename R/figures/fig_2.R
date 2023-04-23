fa0194123c0f0478efea862a8f959ac2ad57b92a
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2023-04-23 21:18:12 +0200

    work on intro and update fig. 2

--- R/figures/fig_2.R
+++ R/figures/fig_2.R
@@ -20,6 +20,29 @@ df$gradient_type <- factor(x = df$gradient_type, levels = c("Extreme Icehouse",
                                                             "Greenhouse",
                                                             "Extreme Greenhouse"))
 df$sample_size <- factor(x = df$sample_size, levels = c(5, 10, 20, "34 (Eocene samples)"))
+
+# Get R2 values
+r2 <- readRDS("./results/simulation/simulated_gradients_r_squared_gelman_unweighted.rds")
+# Rename gradients
+r2$gradient_type[which(r2$gradient_type == "extreme_greenhouse")] <- "Extreme Greenhouse"
+r2$gradient_type[which(r2$gradient_type == "greenhouse")] <- "Greenhouse"
+r2$gradient_type[which(r2$gradient_type == "icehouse")] <- "Icehouse"
+r2$gradient_type[which(r2$gradient_type == "extreme_icehouse")] <- "Extreme Icehouse"
+# Set factor levels
+r2$gradient_type <- factor(x = r2$gradient_type, levels = c("Extreme Icehouse",
+                                                            "Icehouse",
+                                                            "Greenhouse",
+                                                            "Extreme Greenhouse"))
+# Rename sample size
+r2$sample_size[which(r2$sample_size == "eocene")] <- "34 (Eocene samples)"
+r2$sample_size <- factor(x = r2$sample_size, levels = c(5, 10, 20, "34 (Eocene samples)"))
+# Round values
+r2$median <- round(r2$median, 3)
+r2$p_025 <- round(r2$p_025, 3)
+r2$p_975 <- round(r2$p_975, 3)
+r2$value <- paste0("2.5% = ", r2$p_025, "\n", 
+                   "50% = ", r2$median, "\n", 
+                   "97.5% = ", r2$p_975, "")
 # Plot data -------------------------------------------------------------
 col <- "#02818a"
 ggplot(data = df, aes(x = lat, y = median)) +
@@ -31,6 +54,7 @@ ggplot(data = df, aes(x = lat, y = median)) +
               fill = col, alpha = 0.2) +
   geom_line(aes(x = lat, y = known), linetype = 1, colour = "black", size = 0.75, alpha = 1) +
   geom_line(colour = col, linetype = 1, size = 0.75, alpha = 0.9) +
+  geom_text(data = r2, aes(x = 55, y = 38, label = value), hjust = 0, fontface = "bold", size = 2.5) +
   facet_grid(gradient_type~sample_size) +
   scale_x_continuous(limits = c(0, 90), labels = seq(0, 90, 15), breaks = seq(0, 90, 15)) +
   labs(y = "Sea surface temperature (\u00B0C)", x = "Absolute latitude (\u00B0)") +
@@ -43,7 +67,7 @@ ggplot(data = df, aes(x = lat, y = median)) +
         axis.text = element_text(size = 9),
         axis.title = element_text(size = 12, face = "bold"),
         axis.ticks = element_line(colour = "grey70", linewidth = 0.2),
-        panel.grid.major = element_line(colour = "grey70", size = 0.2),
+        panel.grid.major = element_line(colour = "grey70", linewidth = 0.2),
         panel.grid.minor = element_blank())
 # Save plot -------------------------------------------------------------
 ggsave("./figures/fig_2.png",
