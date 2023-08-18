6fe6ad510ae69b70896f27b6541fcba4f91bc6a0
Kilian Eichenseer <kilian.eichenseer@gmail.com>
2023-08-18 16:22:25 +0100

    revised fig 2 script update

--- R/analyses/processing/simulated_gradients_R_squared_revised.R
+++ R/analyses/processing/simulated_gradients_R_squared_revised.R
@@ -6,7 +6,7 @@ source("R/functions/model_components/gradient.R")
 source("R/functions/auxiliary_functions.R")
 
 # read simulated gradient models
-four_gradients_model <- readRDS("D://OneDrive - Durham University/projects/four_gradients_revised_model_proc.rds")
+four_gradients_model <- readRDS("D://OneDrive - Durham University/projects/four_gradients_revised_model_proc_M_wider_noise.rds")
 
 lats <- seq(0,90,1)
 grad <- gradient(lats, four_gradients_model[[1]][[1]])
@@ -18,47 +18,6 @@ gradient_formulas_static <- rev(list( function(x) 30-x^2/1080, # extreme greenho
                                         modern_1deg[sapply(floor(x)+0.5,function(y) which(y==modern_1deg[,"lat"])),2]
                                       }, #gradient(x, c(-1.775, 30.115, 42.070,  0.106)), # now use modern gradient instead # 11.25*atan(-0.1*x+3.5)+15.25, 
                                       function(x) 8.75*atan(-0.5*x+15)+12)) # extreme icehouse
-
-
-plot(lats,gradient_formulas_static[[1]](lats), type = "l")
-points(lats, grad[30000,], col = "orange")
-
-
-# basic R squared
-
-R_squared <- list(NULL)
-
-for(g in 1:4) {
-  R_squared[[g]] <- list(NULL)
-  for(s in 1:4) {
-    grad <- gradient(lats, four_gradients_model[[g]][[s]])
-    true_grad <- gradient_formulas_static[[g]](lats)
-    SSres <- apply(grad,1,function(x) sum((x-true_grad)^2))
-    SStot <- sum((true_grad-mean(true_grad))^2)
-    R_squared[[g]][[s]] <- 1-SSres/SStot
-  }
-}
-
-### Try weighted R squared (by latitudinal area)
-
-lats <- seq(0.5,89.5,1)
-alpha1 <- seq(1,90,1)
-alpha2 <- seq(0,89,1)
-latweight <- sin(pi*alpha1/180) - sin(pi*alpha2/180) # sums to 1 - careful when using lat. subsets
-
-R_squared_weighted <- list(NULL)
-
-for(g in 1:4) {
-  R_squared_weighted[[g]] <- list(NULL)
-  for(s in 1:4) {
-    grad <- gradient(lats, four_gradients_model[[g]][[s]])
-    true_grad <- gradient_formulas_static[[g]](lats)
-    SSres <- apply(grad,1,function(x) sum(latweight*(x-true_grad)^2))
-    SStot <- sum(latweight*(true_grad-mean(true_grad))^2)
-    R_squared_weighted[[g]][[s]] <- 1-SSres/SStot
-    print(c(g,s))
-  }
-}
 
 # Use Gelman's R squared for bayesian regression models
 # http://www.stat.columbia.edu/~gelman/research/unpublished/bayes_R2.pdf
@@ -84,34 +43,7 @@ for(g in 1:4) {
   }
 }
 
-# 
-# 
-# hist(R_squared,1000, xlim = c(0,1))
-# quantile(R_squared[[3]][[1]],probs = c(0.025,0.5,0.975))
-# quantile(R_squared_weighted[[3]][[1]],probs = c(0.025,0.5,0.975))
-# 
-# plot(true_grad, ylim = c(0,45))
-# its <-  sample(1:200000,50)
-# for(i in its) points(grad[i,], col = "blue", type = "l")
-
-# Compare standard R squared and Gelman's R squared
-par(mfrow=c(4,4), mar = c(4,4,.5,.5))
-for(g in 1:4) {
-  for(s in 1:4) {
-    plot(1,quantile(R_squared[[g]][[s]],probs = c(0.5)), ylim = c(0,1.05), cex = 2, lwd = 2, ylab = "R squared", xlab = NA)   
-    points(c(1,1),quantile(R_squared[[g]][[s]],probs = c(0.025,0.975)),type = "l")
-    points(c(1,1),quantile(R_squared[[g]][[s]],probs = c(0.05,0.95)),type = "l", lwd = 2)
-    points(c(1,1),quantile(R_squared[[g]][[s]],probs = c(0.1,0.9)),type = "l", lwd = 3)
-    points(1.1,quantile(R_squared_gelman[[g]][[s]],probs = c(0.5)), col = "red", pch = 5, cex = 1.5, lwd = 2)   
-    points(c(1.1,1.1),quantile(R_squared_gelman[[g]][[s]],probs = c(0.025,0.975)),type = "l", col = "red")
-    points(c(1.1,1.1),quantile(R_squared_gelman[[g]][[s]],probs = c(0.05,0.95)),type = "l", lwd = 2, col = "red")
-    points(c(1.1,1.1),quantile(R_squared_gelman[[g]][[s]],probs = c(0.1,0.9)),type = "l", lwd = 3, col = "red")
-    
-  }
-}
-
-# Gelman's R squared is bound to [0,1], which is desirable --> we use this one.
-
+#
 
 # organise list for figure
 
@@ -142,7 +74,7 @@ names(R_squared_list) <- c("extreme_icehouse","icehouse","greenhouse","extreme_g
 
 # save in long format
 r2_long <- bind_rows(lapply(R_squared_list, function(x) bind_rows(x,.id = "sample_size")),.id="gradient_type")
-saveRDS(r2_long,"./results/simulation/simulated_gradients_r_squared_gelman_unweighted_revised.rds")
+saveRDS(r2_long,"./results/simulation/simulated_gradients_r_squared_gelman_unweighted_revised_M_wider_noise.rds")
 
 # set par mfrow back to 1,1
 par(mfrow = c(1,1))
--- R/figures/fig_2_revised.R
+++ R/figures/fig_2_revised.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(ggthemes)
 library(ggtext)
 # Read data -------------------------------------------------------------
-df <- readRDS("./results/simulation/simulated_gradients_temperature_estimates_with_noise_eocene_resampled_M_wider.rds")
+df <- readRDS("./results/simulation/simulated_gradients_temperature_estimates_with_noise_eocene_resampled_M_wider_noise.rds")
 
 # Rename sample size
 df$sample_size[which(df$sample_size == "eocene")] <- "34 (all Eocene samples)"
@@ -20,7 +20,7 @@ df$gradient_type <- factor(x = df$gradient_type, levels = c("Extreme Icehouse",
 df$sample_size <- factor(x = df$sample_size, levels = c(5, 10, 20, "34 (all Eocene samples)"))
 
 # Get R2 values
-r2 <- readRDS("./results/simulation/simulated_gradients_r_squared_gelman_unweighted_revised_M_wider.rds")
+r2 <- readRDS("./results/simulation/simulated_gradients_r_squared_gelman_unweighted_revised_M_wider_noise.rds")
 # Rename gradients
 r2$gradient_type[which(r2$gradient_type == "extreme_greenhouse")] <- "Extreme Greenhouse"
 r2$gradient_type[which(r2$gradient_type == "greenhouse")] <- "Greenhouse"
@@ -68,5 +68,5 @@ ggplot(data = df, aes(x = lat, y = median)) +
         panel.grid.major = element_line(colour = "grey70"),
         panel.grid.minor = element_blank())
 # Save plot -------------------------------------------------------------
-ggsave("./figures/fig_2_revised_M_wider.png",
+ggsave("./figures/fig_2_revised_M_wider_noise.png",
        units = "mm", width = 200, height = 200, dpi = 600)
